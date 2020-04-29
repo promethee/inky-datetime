65ae6af4fab283b25fac66454f1fa0a0fba60001
promethee <lunette.francois@gmail.com>
2020-04-30 01:39:54 +0200

    Update clock.py

--- clock.py
+++ clock.py
@@ -4,6 +4,8 @@
 import os
 from datetime import date
 from datetime import datetime
+from pytz import timezone
+import pytz
 import argparse
 
 from PIL import Image, ImageDraw, ImageFont
@@ -18,8 +20,7 @@ colour = "black"
 inky_display = InkyPHAT(colour)
 inky_display.set_border(inky_display.BLACK)
 
-fontSmall = ImageFont.truetype("DejaVuSansMono.ttf", 24)
-fontBig = ImageFont.truetype("DejaVuSansMono.ttf", 32)
+fontBig = ImageFont.truetype("DejaVuSansMono.ttf", 40)
 
 # Uncomment the following if you want to rotate the display 180 degrees
 inky_display.h_flip = True
@@ -27,9 +28,9 @@ inky_display.v_flip = True
 
 # Load our backdrop image
 today = date.today()
-now = datetime.now()
+now = datetime.now(tz=timezone("Europe/Paris"))
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
-ImageDraw.Draw(img).text((0, 8), today.strftime(" %B %d"), inky_display.BLACK, font=fontBig)
+ImageDraw.Draw(img).text((0, 8), today.strftime("%B %d"), inky_display.RED, font=fontBig)
 ImageDraw.Draw(img).text((48, 56), now.strftime("%H:%M"), inky_display.BLACK, font=fontBig)
 
 # Display the completed calendar on Inky pHAT
