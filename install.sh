772fe84aa26c2bfc817adcd114c29314a008e8cc
promethee <lunette.francois@gmail.com>
2020-05-02 13:14:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get update -y
 apt-get upgrade -y
-apt-get install python-pip python-pil python-numpy git ntpdate -y
+apt-get install fonts-dejavu python-pip python-pil python-numpy git ntpdate -y
 pip install --upgrade pip setuptools wheel
 pip install inky pytz
 sudo timedatectl set-ntp True
