6c1968039340d001edb03b26fe913cfd821a10e2
promethee <lunette.francois@gmail.com>
2021-08-21 21:23:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get update -y
 apt-get upgrade -y
-apt-get install fonts-dejavu python-pip python-pil python-numpy git ntpdate libopenjp2-7 libtiff5 libgl1-mesa-glx libatlas-base-dev -y
+apt-get install fonts-dejavu python-pip python-rpi.gpio python-pil python-numpy git ntpdate libopenjp2-7 libtiff5 libgl1-mesa-glx libatlas-base-dev -y
 pip install --upgrade pip setuptools wheel
 pip install inky pytz
 sudo timedatectl set-ntp True
